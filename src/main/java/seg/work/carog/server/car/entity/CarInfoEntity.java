b816f119d97d6c915dbac66985bad4d675fb53bd
seoeungi <sjlim02255@gmail.com>
2025-08-03 20:25:23 +0900

    add GeneratedValue annotation

--- src/main/java/seg/work/carog/server/car/entity/CarInfoEntity.java
+++ src/main/java/seg/work/carog/server/car/entity/CarInfoEntity.java
@@ -2,6 +2,8 @@ package seg.work.carog.server.car.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -30,7 +32,7 @@ public class CarInfoEntity extends BaseEntity {
     private static final long serialVersionUID = -1670901009691279360L;
 
     @Id
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
