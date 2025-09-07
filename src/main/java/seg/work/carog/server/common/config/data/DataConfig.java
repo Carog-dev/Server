9ab0626f686d98b3caf326627fc9962d089d1a59
seoeungi <sjlim02255@gmail.com>
2025-09-07 21:04:49 +0900

    불필요 라이브러리, 설정, 코드 제거 및 수정

--- src/main/java/seg/work/carog/server/common/config/data/DataConfig.java
+++ src/main/java/seg/work/carog/server/common/config/data/DataConfig.java
@@ -3,13 +3,8 @@ package seg.work.carog.server.common.config.data;
 import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import java.util.HashMap;
-import java.util.Objects;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
@@ -19,7 +14,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -32,7 +26,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @RequiredArgsConstructor
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "seg.work.carog.server.*.repository")
-@MapperScan(basePackages = "seg.work.carog.server.*.mapper", sqlSessionTemplateRef = "sqlSessionTemplate")
 public class DataConfig {
 
     private final HibernateProperties hibernateProperties;
@@ -95,23 +88,5 @@ public class DataConfig {
         transactionManager.setEntityManagerFactory(entityManagerFactory);
 
         return transactionManager;
-    }
-
-    @Primary
-    @Bean(name = "sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource);
-        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/**/*.xml"));
-        sqlSessionFactoryBean.setTypeAliasesPackage("seg.work.carog.server.*.dto");
-        Objects.requireNonNull(sqlSessionFactoryBean.getObject()).getConfiguration().setMapUnderscoreToCamelCase(true);
-
-        return sqlSessionFactoryBean.getObject();
-    }
-
-    @Primary
-    @Bean(name = "sqlSessionTemplate")
-    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
